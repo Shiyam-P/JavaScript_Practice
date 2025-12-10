44f953963d9b1a5277b080673e843511303f792d
Shiyam-P <shiyam9904@gmail.com>
2025-12-10 23:17:02 +0530

    create a file name as conditionalstmt.html and conditionalstmt.js

---
+++ conditionalstmt.js
@@ -0,0 +1,17 @@
+// if stmt
+
+let learnhtml = "HTML";
+let learncss = "CSS";
+let learnjs = "JavaScript";
+let allthree = "Web Developer";
+
+let live = "learncss";
+if(learnhtml){
+    console.log(`valthukkol you are ${learnhtml} developer`);
+}
+else if(learncss){
+    console.log(`valthukkol you are ${learncss} developer`);
+}
+else{
+    console.log("you are not a developer");
+}
