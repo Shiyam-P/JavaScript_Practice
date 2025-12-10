df3eeebc74794ac007eeca947a5499b88de54d4d
Shiyam-P <shiyam9904@gmail.com>
2025-12-10 23:39:51 +0530

    To modify the conditionalstmt.js file

--- conditionalstmt.js
+++ conditionalstmt.js
@@ -1,17 +1,25 @@
 // if stmt
 
-let learnhtml = "HTML";
-let learncss = "CSS";
-let learnjs = "JavaScript";
-let allthree = "Web Developer";
+let learnhtml = true;
+let learncss = true;
+let learnjs = true;
+let unknown = false;
 
-let live = "learncss";
-if(learnhtml){
-    console.log(`valthukkol you are ${learnhtml} developer`);
+if(unknown){
+    console.log(`Unknown Entry`);
+}
+else if(learnhtml && learncss && learnjs){
+    console.log(`valthukkol you are Web developer`);
+}
+else if(learnhtml){
+    console.log(`valthukkol you are HTML developer`);
 }
 else if(learncss){
-    console.log(`valthukkol you are ${learncss} developer`);
+    console.log(`valthukkol you are CSS developer`);
+}
+else if(learnjs){
+    console.log(`valthukkol you are JavaScript developer`);
 }
 else{
-    console.log("you are not a developer");
+    console.log("Sorry! you are not a developer");
 }
