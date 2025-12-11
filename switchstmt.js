f4dda0a9185ffa929b307dd1a2fbb259900f310f
Shiyam-P <shiyam9904@gmail.com>
2025-12-11 22:33:47 +0530

    practice the switchstmt

---
+++ switchstmt.js
@@ -0,0 +1,88 @@
+// Switch stmt
+
+switch(Math.floor(Math.random() * 4)){
+    case 1:
+        console.log("Print 1");
+        break;
+    case 2:
+        console.log("Print 2");
+        break;
+    default:
+        console.log("Print Nothing");  
+}
+
+// rock, paper, scissors
+let player = "rock";
+let computer = "rock";
+
+switch(player){
+    case computer:
+        console.log("Tie");
+        break;
+
+    case "rock":
+        if(computer === "paper"){
+            console.log("computer is win");
+        }
+        else{
+            console.log("player is win");
+        }
+        break;
+    
+    case "paper":
+        if(computer === "scissor"){
+            console.log("computer is win");
+        }
+        else{
+            console.log("player is win");
+        }
+        break;
+
+    case "scisser":
+        if(computer === "rock"){
+            console.log("computer is win");
+        }
+        else{
+            console.log("player is win");
+        }
+        break;
+    
+    default:
+        console.log("Invalid choice");   
+}
+
+let chatplayer = "no";
+let chatcomputer = "rock";
+
+switch(chatplayer){
+    case chatcomputer:
+        console.log("Tie");
+        break;
+
+    case "scissor":
+        if(chatcomputer === "paper"){
+            console.log("player win");
+        } else {
+            console.log("computer win");
+        }
+        break;
+
+    case "rock":
+        if(chatcomputer === "scissor"){
+            console.log("player win");
+        } else {
+            console.log("computer win");
+        }
+        break;
+
+    case "paper":
+        if(chatcomputer === "rock"){
+            console.log("player win");
+        } else {
+            console.log("computer win");
+        }
+        break;
+
+    default:
+        console.log("Invalid choice");
+}
