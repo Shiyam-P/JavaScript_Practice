f11478ba276712bd4b80fc5c1519dab5ed25a5c8
Shiyam-P <shiyam9904@gmail.com>
2025-12-12 21:14:06 +0530

    modify the userinput.js file

--- userinput.js
+++ userinput.js
@@ -1,2 +1,21 @@
-let boolean = alert("vanakkam")
-console.log(boolean);
+// // alert
+// let boolean = alert("hello");
+// console.log(boolean);
+
+// // prompt
+// let boolean = prompt("Enter your name: ")
+// console.log(boolean);
+
+// // Nullish coalescing operator (??)
+// let myName = prompt("Enter your name: ");
+// console.log(myName ?? "You didn't enter your name");
+
+
+let myName = prompt("Enter your name: ");
+if(myName){
+    console.log(myName ?? "You didn't enter your name");
+}
+else{
+    console.log("You didn't enter your name"); 
+}
+console.log(myName.trim().length);
