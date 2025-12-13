83562962a6d31865c50a8483adcd5253a3dca8e2
Shiyam-P <shiyam9904@gmail.com>
2025-12-13 22:41:07 +0530

    finish the largest of two numbers

---
+++ example/ex2.js
@@ -0,0 +1,13 @@
+let value1 = 82;
+let value2 = 83;
+
+if(value1 === value2){
+    console.log("Both numbers are equals");   
+}
+else if(value1 >=  value2){
+    console.log("value1 is greater");
+}
+else{
+    console.log("value2 is greater");
+}
+
