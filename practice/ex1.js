0d1067d42bf03fe347673451eef6eb2009883faf
Shiyam-P <shiyam9904@gmail.com>
2025-12-16 00:17:32 +0530

    finish the vowel or consonant program

---
+++ practice/ex1.js
@@ -0,0 +1,11 @@
+// Check whether a given character is a vowel or consonant.
+
+let character = "A";
+
+if(character === "a" || character === "e" || character === "i" || character === "o" || character === "u" ||
+    character === "A" || character === "E" || character === "I" || character === "O" || character === "U"){
+    console.log(character + " is vowel"); 
+}
+else{
+    console.log(character + " is consonant");
+}
