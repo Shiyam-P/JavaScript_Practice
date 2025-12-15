cfd0e7c8e2f299225b9c2ccfcf18800f805b9605
Shiyam-P <shiyam9904@gmail.com>
2025-12-16 00:40:01 +0530

    create and modify the file

---
+++ practice/ex2.js
@@ -0,0 +1,25 @@
+// Use a switch statement to print the day name based on day number.
+
+let day = 6;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thuresday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid Number");
+}
